d98c49a6e6e0a604c108da44459eb9272b859285
Amit Ambardekar <amitamb@gmail.com>
2014-01-17 19:50:34 +0530

    Undoing some changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact
-  include ActiveAttr::Model
+  include ActiveModel::Model
 
   attr_accessor :nickname, 
                 :name, 
@@ -13,23 +13,23 @@ class Contact
   validates :name, 
             :subject, 
             presence: true
-   
+
   validates :email,
             format: { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
-   
+
   validates :message,
             length: { :minimum => 10, :maximum => 1000 }
-  
+
   # def initialize(attributes = {})
   #   attributes.each do |name, value|
   #     send("#{name}=", value)
   #   end
   # end
-  
+
   def deliver
     true
   end
-      
+
   # def persisted?
   #   false
   # end
